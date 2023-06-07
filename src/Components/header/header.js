62aa6ff9d3a7f1623b30bc8e048ea26016ea6772
Mavroos <maurotonetti2k22@gmail.com>
2023-06-07 22:16:54 +0200

    second commit

--- src/Components/header/header.js
+++ src/Components/header/header.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './header.css';
 
 
@@ -17,24 +16,4 @@ function header() {
     );
   }
   
-=======
-import './header.css';
-
-
-
-function header() {
-    return (
-        <div className="header">
-            <h1 className="myName"> Mauro Tonetti </h1>
-            <ul className="lista">
-                <li><a className="element" href="#Home">Home</a></li>
-                <li><a className="element" href="#Skills">Skills</a></li>
-                <li><a className="element" href="#Projects">Projects</a></li> <a></a>
-            </ul>
-        </div>
-      
-    );
-  }
-  
->>>>>>> bb2d0a04ce3473edcc3520ef13ae46eefad06df3
   export default header;
